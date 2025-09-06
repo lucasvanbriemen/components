de66235ddec113039d53dbfd4976ffbec13654d0
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-09-06 17:00:21 +0200

    use seprate file

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
 
-// Return colors.json
 Route::get("/colors", function () {
-    $path = resource_path("json/colors.json");
-    if (!File::exists($path)) {
-        return response()->json(["message" => "Colors file not found."], 404);
-    }
-    $colors = File::get($path);
-    return response($colors, 200)->header("Content-Type", "application/json");
+  $theme = request()->query("theme");
+
+  if (!$theme) {
+    return response()->json(["message" => "Missing theme parameter."], 400);
+  }
+
+  $path = resource_path("json/{$theme}.json");
+
+  if (!File::exists($path)) {
+    return response()->json(["message" => "Theme not found."], 404);
+  }
+
+  $colors = File::get($path);
+
+  return response($colors, 200)
+    ->header("Content-Type", "application/json");
 });
