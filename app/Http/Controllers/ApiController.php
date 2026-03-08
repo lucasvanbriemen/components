2b29bdb4c2adb5dc3df65783dc5678cc0173041f
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-03-08 11:00:31 +0100

    Get colors from API controller.

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class ApiController extends Controller
+{
+    public function getThemeColors(Request $request)
+    {
+        $theme = request()->query("theme");
+
+        if (!$theme) {
+            return response()->json(["message" => "Missing theme parameter."], 400);
+        }
+
+        $path = resource_path("json/colors/{$theme}.json");
+
+        if (!File::exists($path)) {
+            return response()->json(["message" => "Theme not found."], 404);
+        }
+
+        $colors = File::get($path);
+
+        return response($colors, 200)
+        ->header("Content-Type", "application/json");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\File;
+use App\Http\Controllers\ApiController;
 
-Route::get("/colors", function () {
-    $theme = request()->query("theme");
-
-    if (!$theme) {
-        return response()->json(["message" => "Missing theme parameter."], 400);
-    }
-
-    $path = resource_path("json/colors/{$theme}.json");
-
-    if (!File::exists($path)) {
-        return response()->json(["message" => "Theme not found."], 404);
-    }
-
-    $colors = File::get($path);
-
-    return response($colors, 200)
-      ->header("Content-Type", "application/json");
-});
+Route::get("/colors", [ApiController::class, "getThemeColors"]);
